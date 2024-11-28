09036dba083dd24dd32dbf2165322f3c5c25db15
Aqsa <hafizaaqsa42@gmail.com>
2024-11-28 12:12:59 +0500

    remove lint errors

--- .history/.eslintrc_20241128120317.js
+++ .history/.eslintrc_20241128120317.js
@@ -2,7 +2,6 @@ module.exports = {
   'env': {
     'browser': true,
     'es6': true,
-    'node': true,
     'jest/globals': true
   },
   'extends': [
@@ -48,4 +47,3 @@ module.exports = {
     'react/prop-types': 0
   }
 }
-
